cc50c8534af7e23414b645e475d26ccbc41bf4d1
marina <marina.fsiq@gmail.com>
2019-01-31 21:04:56 -0200

    Adding default files after creating the project in STS

---
+++ src/main/java/com/marinafsiq/bankStatmentControl/api/BankStatmentControlApplication.java
@@ -0,0 +1,14 @@
+package com.marinafsiq.bankStatmentControl.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatmentControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatmentControlApplication.class, args);
+	}
+
+}
+
