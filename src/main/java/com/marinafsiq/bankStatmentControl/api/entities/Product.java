797306034e51274ae12223bf4c4132a170ef673b
marina <marina.fsiq@gmail.com>
2019-02-19 21:15:22 -0300

    - Removing unnecessary imports - Adding pass to MySQL database

--- src/main/java/com/marinafsiq/bankStatmentControl/api/entities/Product.java
+++ src/main/java/com/marinafsiq/bankStatmentControl/api/entities/Product.java
@@ -3,17 +3,12 @@ package com.marinafsiq.bankStatmentControl.api.entities;
 import java.io.Serializable;
 
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
